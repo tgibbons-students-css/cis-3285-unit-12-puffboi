3ae55bf2832fd713f605ea9adb87f050d6d70281
puffboi <spohlmann@css.edu>
2016-11-22 11:04:19 -0600

    Changes made to program class

--- Console/Program.cs
+++ Console/Program.cs
@@ -13,13 +13,21 @@ namespace ConsoleSample
         static void Main(string[] args)
         {
 
-            EntityRepository entityRepository = new EntityRepository();
+            IEntityRepository<Entity> entityRepository = new EntityRepository();
             Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
             myEntity.Name = "Tom";
             Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
             Console.WriteLine("New entity Name: " + myEntity.Name);
+    
+            IEntityRepository<User> userRepository = new UserRepository();
+            User myUser = userRepository.GetByID(Guid.NewGuid());
 
+            myUser.Name = "Daniel";
+            myUser.EmailAddress = "dblasena@css.edu";
+            Console.WriteLine("New user ID: " + myUser.ID.ToString());
+            Console.WriteLine("New user Name: " + myUser.Name);
+            Console.WriteLine("New user Email Address: " + myUser.EmailAddress);
         }
     }
 }
